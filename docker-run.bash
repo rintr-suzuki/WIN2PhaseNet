39d25ddcc54d6dde8142380fefabe7384f6b3558
rintr-suzuki <plumarine.trip@gmail.com>
2024-01-23 18:06:59 +0900

    [fix] bug fix of docker-run.bash

--- docker-run.bash
+++ docker-run.bash
@@ -5,8 +5,7 @@ name=$1
 
 ## check OS
 OSname="Mac-Linux"
-which ipconfig 2>&1 >/dev/null # only exists Windows
-if [[ $? == 0 ]]; then # check exit code
+if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "win32" ]]; then
     OSname="Windows"
 fi
 
